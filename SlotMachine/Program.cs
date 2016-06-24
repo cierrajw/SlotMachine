631ba89d0bbd6cb253ca569b690b621faa0c1151
cierrajw <cw1572@nyu.edu>
2016-06-24 11:32:43 -0400

    Editing methods, etc.

---
+++ SlotMachine/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine
+{
+    class Program
+    {
+        /*
+         * Welcome message
+         * Directions
+         * Choose a machine
+         * Place a bet
+         * Pull lever
+         * Randomly rolls around and gives you 
+         * Machine tells you if you won or not
+         */
+        static void Main(string[] args)
+        {
+            SlotMachine myMachine = new SlotMachine(); // 3 slots, 5 icons per slot
+
+            Console.WriteLine("Welcome to slots!");
+
+            Console.WriteLine("Directions");
+
+            while (true)
+            {
+                // place a bet
+                Console.WriteLine("Type in how many pennies to bet");
+
+                // You could get this using:
+                // int userBet = Convert.ToInt32(Console.ReadLine());
+                // myMachine.CurrentBet = userBet;
+                myMachine.CurrentBet = Convert.ToInt32(Console.ReadLine());
+
+                // pull the lever
+                Console.WriteLine("Press enter to pull the lever");
+                Console.ReadKey(); // TODO Later: make this actually look for ENTER
+                myMachine.PullLever();
+
+                // display the results
+                int[] tempResults = myMachine.GetResults();
+                for (int i = 0; i < tempResults.Length; i++)
+                {
+                    Console.Write(tempResults[i] + " ");
+                }
+
+                // payout
+                Console.WriteLine("You won {0} pennies!", myMachine.GetPayout());
+            }
+
+        }
+    }
+}
+
+
+
+
+
---
+++ SlotMachine/SlotMachine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine
+{
+    class SlotMachine
+    {
+        public int NumberOfSlots { get; set; }
+
+        public int IconsPerSlot { get; set; }
+        public int MinimumBet { get; set; }
+        public int MaximumBet { get; set; }
+
+
+        private int _currentBet;
+        public int CurrentBet
+        {
+            get
+            {
+                return _currentBet;
+            }
+            set
+            {
+                _currentBet = value;
+                if (_currentBet < MinimumBet)
+                {
+                    _currentBet = MinimumBet;
+                }
+                if (_currentBet > MaximumBet)
+                {
+                    _currentBet = MaximumBet;
+                }
+            }
+        }
+
+        /// <summary>
+        /// An array of integers that is as long as the number of slots,
+        /// with each element of the array representing a different slot
+        /// </summary>
+        private int[] icons;
+
+
+        public SlotMachine()
+        {
+            NumberOfSlots = 3;
+            IconsPerSlot = 5;
+            MinimumBet = 1;
+            MaximumBet = 100;
+        }
+
+        /// <summary>
+        /// Randomizes the contents of the icons
+        /// </summary>
+        public void PullLever()
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Return the results from the slots
+        /// </summary>
+        /// <returns>an int[] with each slot as an element of the array</returns>
+        public int[] GetResults()
+        {
+            // TODO
+            return null;
+        }
+
+        /// <summary>
+        /// Examine the contents of the slots and determine the appropriate payout
+        /// based upon the CurrentBet.
+        /// </summary>
+        /// <returns>number of pennies to pay out</returns>
+        public int GetPayout()
+        {
+            // TODO
+            return 0;
+        }
+
+
+
+    }
+}
